5f5a5c3ed008e76de5b16f2390cfe14b35270ae5
Mellinger <mellinger@iu13.org>
2019-04-18 15:21:56 -0400

    Error handling

--- demo.ps1
+++ demo.ps1
@@ -28,7 +28,7 @@ Get-Help Update-Help
 Get-Command *eventlog*
 
 #Note the command names returned. These follow the Verb-Noun structure which make them fairly easy to read and understand. Get-, Clear-, New-, Remove- are all actions that you recognize.
-#Most commands follow this structure, even third party modules. 
+#Most commands follow this structure, even third party modules. You can check and see the full list of "approved" verbs by running Get-Verb. Nothing forces people to use those verbs. 
 
 #Get-EventLog is an easily acccessible command. Look up the help files for Get-EventLog. Note that these give two different sets of Syntax that can be used. This shows the types of input that
 #can be accepted. Each of these are parameters that can be added to the command which we will cover in a bit. It also gives a description and links to the online version. 
@@ -73,7 +73,54 @@ Get-EventLog System -Newest 10 -EntryType Error -ComputerName "TTL1.hq.iu13.loca
 
 #endregion
 
+#region Aliases
+#Aliases can be used to shorten the name of common commands. You can find if there is an alias using Get-Alias
+Get-Alias -Definition "Get-Member"
 
+#You can reverse this if you find an alias in a script online. Use Get-Help and it will show you the actual command behind the alias
+Get-Help gm
+
+#There are aliases for other commands that you wouldn't expect. Dir is a cmd command to display items in the directory. The Powershell command is Get-ChildItem
+Get-Help dir
+
+#Also unxpected aliases for Get-ChildItem from other operating systems. 
+Get-Help ls
+
+#We can see that gci is another alias as well
+Get-Alias -Definition "Get-ChildItem"
+
+#You can create aliases yourself using New-Alias but I wouldn't recommend it. The aliases are only available to you and not ported to any other computer unless you use Export-Alias. 
+#Aliases are discouraged in scripts. They are easy to type when you're just running a command, but a script you're likely to come back to or someone else will read it.
+#In that case aliases make it harder to understand the commands. It's advised to just type out Get-ChildItem. After all, that's what tab completion is for, right?
+#endregion
+
+#region Making things easy
+#Ok so you see how to find parameters using help and how to use them. That's hard to grasp at first though. Plus for some people working in a command line only can get tiring.
+#Sometimes you just want to click something, right? (We are Windows admins after all). Show-Command is helpful and gives you boxes to fill out. Once you fill them out you can click Run
+#Your command is then populated with all the parameters filled out. This can be useful for learning 
+Show-Command Get-EventLog
+
+#endregion
+
+#region Errors
+#Errors will happen. I run into them all the time whether I'm mistyping something or just running the wrong command. The biggest thing is learning how to read them. 
+#Powershell for the most part has errors that make sense, it's just taking the time to read and understand them. 
+
+#Bad Command. We can see that this isn't a command. Good time to use Get-Command and see if we just didn't type the right name
+#The term 'Get_Service' is not recognized as the name of a cmdlet, function, script file, or operable program
+Get_Service
+
+#This can happen with parameters that need to be in certain formats. If you check Get-Date, the -Date parameter needs to be a Date/Time format. We're specifying it using a bad date and time
+#You can see that Powershell attempts to convert it to the correct format. We can even feed it dates that aren't in that format ("May 1") and it will convert to date/time. However this is
+#bad enough it won't work
+#Cannot bind parameter 'Date'. Cannot convert value "May_1" to type "System.DateTime"
+Get-Date -Date "May_1"
+
+#Don't be scared of errors. They happen often enough especially as you are learning. Just take the time to read them and learn what they're saying. If you need to, Google them and there's often examples
+#Worst case as a presenter once said, you can change the error text color to Green. It makes you feel a lot less like red ink on a test
+$host.PrivateData.ErrorForegroundColor = 'Green'
+
+#endregion
 #endregion
 
 #region Intermediate
